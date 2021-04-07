d1c06a3664c761660df60a805a7ec94d989084ae
efrencz <82116383+efrencz@users.noreply.github.com>
2021-04-07 13:48:53 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,25 @@
+<?php
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'efrencarrasquilla@gmail.com';
+$asunto = 'Mensaje de PRUEBA';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
+-->
